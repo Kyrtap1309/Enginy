177b99933c179f663044a215089f68daaa4d2bc2
Patryk Niczke <kyrtap1301@gmail.com>
2024-10-09 17:55:16 +0200

    Add ISA temperature calculation

--- Enginy/isa/isa.py
+++ Enginy/isa/isa.py
@@ -17,19 +17,20 @@ g_st = 9.8067  # Standard Acceleration of Gravity [m/s2]
 L = -6.5 / 1000  # Temperature ISA gradient [K/m]
 H_top_troposphere = 11_000  # ISA Top of Troposphere [m]
 T_st = 288.15  # ISA Temperature on the 0 level [K]
-p_st = 101325  # ISA Pressure on the 0 level [Pa]
+p_st = 101_325  # ISA Pressure on the 0 level [Pa]
 
 ### Stratosphere
 H_bottom_stratosphere = H_top_troposphere + 0.01  # ISA Bottom of Stratosphere
 H_top_stratosphere = 47_000  # ISA Top of Stratosphere [m]
 T_bottom_stratosphere = 216.65  # Temperature at the bottom of Stratosphere [K]
-p_bottom_stratosphere = 22632.06  # Pressure at the bottom of Stratosphere [Pa]
-delta_bottom_strato = p_bottom_stratosphere / p_st
+p_bottom_stratosphere = 22_632.06  # Pressure at the bottom of Stratosphere [Pa]
+delta_bottom_strato = p_bottom_stratosphere / p_st #ISA Delta value at the bottom of Stratosphere
+theta_bottom_strato = T_bottom_stratosphere / T_st #ISA Theta value at the bottom of Stratosphere
 
 
 def ISA_delta(H: float, unit="meter") -> float:
     """
-    Calucate ISA delta, the ISA pressure ratio, for a given pressure altitude
+    Calucate ISA delta: the ISA pressure ratio, for a given pressure altitude
     limited to top of stratosphere
     args:
         H: Absolute height with unit declared in unit variable
@@ -71,3 +72,28 @@ def ISA_p(H: float, unit="meter") -> float:
     
     pressure = p_st * ISA_delta_vectorize(H)
     return pressure
+
+def ISA_theta(H: float, unit="meter") -> float:
+    """
+    Calucate ISA theta: the ISA temperature ratio, for a given temperature altitude
+    limited to top of stratosphere
+    args:
+        H: Absolute height with unit declared in unit variable
+    return:
+        temperature: Air temperature at input height according to ISA [K]
+    """
+
+    if unit not in CONVERT_TO_M:
+        raise ValueError("Invalid input unit")
+
+    H *= CONVERT_TO_M[unit]
+
+    if H <= H_top_troposphere:
+        temperature = (1 + (L / T_st) * H)
+        return temperature
+    elif H <= H_top_stratosphere:
+        temperature = theta_bottom_strato
+        return temperature
+    else: 
+        raise ValueError("Altitude above stratospheric limit")
+    
