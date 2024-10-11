b552a1caf7513493619c080b2134a4315f768a00
Patryk Niczke <kyrtap1301@gmail.com>
2024-10-11 12:28:46 +0200

    Add static pressure and heat capacity ratio functions

--- Enginy/engine_parts/engine_thermo.py
+++ Enginy/engine_parts/engine_thermo.py
@@ -1,3 +1,5 @@
+import cantera as ct
+
 def get_p_total(p_static: float,
                 gamma: float,
                 M:float) -> float:
@@ -42,3 +44,33 @@ def get_T_static(T_total: float,
     """
     T_static = T_total / (1 + ((gamma - 1) / 2) * M**2)
     return T_static
+
+def get_p_static(p_total: float,
+                 T_static: float,
+                 T_total: float,
+                 gamma: float,
+                 ) -> float:
+    """
+    Calculate the static pressure for isentropic process
+    args:
+        p_total: stagnatic pressure of a gas 
+        T_static: Static temperature of a gas [K] 
+        T_total: Stagnation temperature of a gas [K]
+        gamma: heat capacity ratio
+        M: Mach number of a gas
+    return:
+        p_static: Static pressure of a gas
+    """
+    p_static = p_total * (T_static/T_total) ** (gamma/(gamma - 1))
+    return p_static
+
+def get_gamma(gas: ct.Solution) -> float:
+    """
+    Calculate a heat capacity ratio
+    args:
+        gas: cantera.Solution object which represents gas or mixture of gases
+    return:
+        gamma: het capacity ratio of a gas
+    """
+    gamma = gas.cp / gas.cv
+    return gamma
