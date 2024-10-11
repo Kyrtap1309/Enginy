a2233d52e8f3f937dc73443348871ec97fe1fda2
Patryk Niczke <kyrtap1301@gmail.com>
2024-10-11 15:07:28 +0200

    Start typing function to calculate mach number at the end of inlet

--- Enginy/engine_parts/inlet.py
+++ Enginy/engine_parts/inlet.py
@@ -1,6 +1,29 @@
+import json
 
+from ..isa import isa 
+from ..engine_parts import engine_thermo
 
 class Inlet:
     def __init__(self, inlet_json):
-        """_summary_
         """
+        Constructor of inlet of aircraft jet engine
+        Args:
+            inlet_json (json): Json file with all engine params
+        """
+        with open(inlet_json, 'r') as file:
+            self.inlet_data = json.load(file)
+    
+    def mach_inlet(self) -> tuple[float, bool]:
+        """
+        Calculate gas velocity at the end of inlet
+
+        Return:
+            M_out: Mach number for gas at the end of inlet
+            convergence: True if calculations are converged
+        """
+        #Calculations params
+        max_iterations = 100 #Maximum number of iterations
+        tol = 0.01 #Toleration for convergence
+        n_iter = 0 #iteration counter
+
+        M_input = self.inlet_data["M_inlet_input"]
