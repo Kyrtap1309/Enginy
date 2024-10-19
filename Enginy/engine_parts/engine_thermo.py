99c3a349bbb00088e91e1cd3613fb864ee8e6a37
Patryk Niczke <kyrtap1301@gmail.com>
2024-10-19 11:37:46 +0200

    Move mach solver to engine thermodynamic module

--- Enginy/engine_parts/engine_thermo.py
+++ Enginy/engine_parts/engine_thermo.py
@@ -99,3 +99,60 @@ def get_a(gas: ct.Solution) -> float:
     """
     a = np.sqrt(get_R(gas) * get_gamma(gas) * gas.T)
     return a
+
+
+def mach_solver(
+    mass_flow: float,
+    A: float,
+    gas_in: ct.Solution,
+    eta: float,
+    Ma_in: float,
+    gas_out: ct.Solution,
+    max_iterations: int = 100,
+    tol: float = 0.01,
+) -> tuple[float, bool]:
+    """
+        Calculate gas velocity at the end of engine part
+
+        args:
+            mass_flow: mass flow of gas in engine part [kg/s]
+            A: Cross-sectional area [m^2]
+            gas_in: Gas at entrance of engine part
+            eta: engine part thermodynamic efficiency
+            Ma_in: Mach number at the entrance of engine part
+            gas_out: Gas at exit of engine part
+            max_iterations: maximum iterations of algorithm
+            tol: tolerance of calculation of gas velocity at the end
+        Return:
+            M_out: Mach number for gas at the end of inlet
+            convergence: True if calculations are converged
+    """
+    n_iter = 0 #iteration counter
+    converged = False #Tracking of convergence
+    
+    # calculated input gas properties
+    V_in = Ma_in * get_a(gas_in)
+    gamma_in = get_gamma(gas_in)
+    T_total_in = get_T_total(gas_in.T, gamma_in, Ma_in)
+    p_total_in = get_p_total(gas_in.P, gamma_in, Ma_in)
+
+    # initial assumptions
+    T_total_out = T_total_in
+    V_out_guess = mass_flow / (gas_in.density * A)
+    gamma_out = gamma_in
+    
+    while not converged and n_iter <= max_iterations:
+        
+        # calc properties using current guess
+        
+        T_static_out = gas_in.T + (V_in**2 / (2 * gas_in.cp) - V_out_guess**2 / (2 * gas_out.cp))
+        p_total_out = gas_in.P * (1 + eta * V_in**2 / (2 * gas_in.cp * gas_in.T))**(gamma_in / (gamma_in - 1))
+        p_static_out = p_total_out * (T_total_out / T_static_out)**(gamma_out / (gamma_out - 1))
+              
+        # update gas to get new properties (especially density)
+        gas_out.TP = T_static_out, p_static_out
+        gamma_out = get_gamma(gas_out)
+        
+        # update velocity calculation with new gas properties
+        V_out = V_out_guess
+        V_out_guess = mass_flow / (gas_out.density * A)
