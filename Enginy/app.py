47c9097ac9f2b7506842f2e96fddb24634912eb1
Patryk Niczke <kyrtap1301@gmail.com>
2024-10-21 18:38:51 +0200

    Add plotting to flask app

--- Enginy/app.py
+++ Enginy/app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 
+from .engine_parts.inlet import Inlet
 from .utils.constants import available_parts
+
 
 app = Flask(__name__)
 
-# Example engine parts
+#Engine parts
 engine_parts = []
+
+#Engine parts mapping
+engine_parts_classes = {
+    "Inlet": Inlet,
+}
 
 # Main Page
 @app.route('/')
@@ -16,9 +23,15 @@ def index():
 @app.route('/create_part', methods=['GET', 'POST'])
 def create_part():
     if request.method == 'POST':
-        part_name = request.form['part_name']
+        data = request.form.to_dict()
+        # Float convertion
+        for key in data:
+            if key != "part_name":
+                data[key] = float(data[key])
+        part_name = request.form["part_name"]
+        part = engine_parts_classes[part_name](data)
         analysis_result = f"Analysis for {part_name}"  # Analysis' Logic
-        engine_parts.append({'name': part_name, 'analysis': analysis_result})
+        engine_parts.append({'part': part, 'name': part_name, 'analysis': analysis_result})
         return redirect(url_for('index'))
     
     return render_template('create_part.html', available_parts=available_parts)
@@ -31,6 +44,13 @@ def delete_part(part_index):
     
     return redirect(url_for('index'))
 
+@app.route('/analyze_part/<int:part_index>', methods=['GET'])
+def analyze_part(part_index):
+    if 0 <= part_index < len(engine_parts):
+        analysis = engine_parts[part_index]["part"].analyze()
+        return render_template("analyze.html", analysis=analysis)
+    
+
 # Analysis of whole engine
 @app.route('/analyze_engine', methods=['POST'])
 def analyze_engine():
@@ -39,6 +59,7 @@ def analyze_engine():
     # Logic of engine analysis
     engine_analysis_result = "Complete Engine Analysis"
     return jsonify({'result': engine_analysis_result})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Enginy/engine_parts/inlet.py
+++ Enginy/engine_parts/inlet.py
@@ -1,5 +1,5 @@
 import json
-
+from plotly import utils
 
 from ..isa import isa
 from . import engine_thermo
@@ -7,18 +7,16 @@ from . import gas_management
 
 
 class Inlet:
-    def __init__(self, inlet_json):
+    def __init__(self, inlet_data):
         """
         Constructor of inlet of aircraft jet engine
-        Args:
-            inlet_json (json): Json file with all engine params
+    
         """
-        with open(inlet_json, "r") as file:
-            self.inlet_data = json.load(file)
+        self.inlet_data = inlet_data
 
         self.T_ambient = isa.ISA_T(self.inlet_data["altitude"])
         self.p_ambient = isa.ISA_p(self.inlet_data["altitude"])
-        self.M_ambient = self.inlet_data["M_inlet_input"]
+        self.M_ambient = self.inlet_data["M_ambient_input"]
         self.mass_flow = self.inlet_data["mass_flow"]
         self.A_1 = self.inlet_data["A1"]
         self.A_2 = self.inlet_data["A2"]
@@ -28,7 +26,7 @@ class Inlet:
         _ambient_stage_index = gas_management.st[0]
         self.ambient_stage_gas = self.gas[_ambient_stage_index]
 
-        self.v_input = self.M_input * engine_thermo.get_a(self.ambient_stage_gas)
+        self.v_input = self.M_ambient * engine_thermo.get_a(self.ambient_stage_gas)
         self.gamma = engine_thermo.get_gamma(self.ambient_stage_gas)
         self.T_total_inlet_in = engine_thermo.get_T_total(
             self.ambient_stage_gas.T, self.gamma, self.M_ambient
@@ -42,6 +40,9 @@ class Inlet:
 
         _inlet_out_stage_index = gas_management.st[2]
         self.inlet_out_stage_gas = self.gas[_inlet_out_stage_index]
+
+        self.mach_inlet_from_ambient_to_entrance()
+        self.mach_inlet_from_entrance_to_output()
 
     def mach_inlet_from_ambient_to_entrance(self):
         M_calc, convergence = engine_thermo.mach_solver(
@@ -69,14 +70,14 @@ class Inlet:
         if convergence:
             self.M_inlet_out = M_calc
 
-    def plot_T_s(self):
+    def analyze(self):
         inlet_T = []
         inlet_p = []
         inlet_X = []
 
         for x in range(0, 3):
             inlet_T.append(self.gas[gas_management.st[x]].T)
-            inlet_p.append(self.gas[gas_management.st[x]].p)
+            inlet_p.append(self.gas[gas_management.st[x]].P)
             inlet_X.append(self.gas[gas_management.st[x]].X)
 
         plot = gas_management.plot_T_s(
@@ -86,4 +87,6 @@ class Inlet:
             gas_management.reaction_mechanism,
             gas_management.phase_name,
         )
-        return plot
+
+        graphJSON = json.dumps(plot, cls = utils.PlotlyJSONEncoder)
+        return graphJSON
