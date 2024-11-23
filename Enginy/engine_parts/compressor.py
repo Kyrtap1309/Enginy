75eb6ca35fbc3b681e46d647dc31e0bfdfff1caf
Patryk Niczke <kyrtap1301@gmail.com>
2024-11-23 13:26:38 +0100

    Add plotting method

--- Enginy/engine_parts/compressor.py
+++ Enginy/engine_parts/compressor.py
@@ -1,3 +1,6 @@
+import json
+from plotly import utils
+
 from .engine_part import EnginePart
 from . import gas_management, engine_thermo
 from .inlet import Inlet
@@ -25,6 +28,27 @@ class Compressor(EnginePart):
             comp_eta=self.comp_eta,
             M_in=self.M_comp_in,
             gas_out=self.gas["3"])
+        
+    def analyze(self):
+        compressor_T = []
+        compressor_p = []
+        compressor_X = []
+
+        for x in range(0, 4):
+            compressor_T.append(self.gas[gas_management.st[x]].T)
+            compressor_p.append(self.gas[gas_management.st[x]].P)
+            compressor_X.append(self.gas[gas_management.st[x]].X)
+
+        plot = gas_management.plot_T_s(
+            compressor_T,
+            compressor_p,
+            compressor_X,
+            gas_management.reaction_mechanism,
+            gas_management.phase_name,
+        )
+
+        graphJSON = json.dumps(plot, cls = utils.PlotlyJSONEncoder)
+        return graphJSON
 
                                                                                          
 
