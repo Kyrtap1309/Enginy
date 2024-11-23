7ec37504dd45c64e5d977d8cbe8a3af1b679fe92
Patryk Niczke <kyrtap1301@gmail.com>
2024-11-23 13:23:45 +0100

    Add constructor to Compressor class

--- Enginy/engine_parts/compressor.py
+++ Enginy/engine_parts/compressor.py
@@ -1,10 +1,31 @@
 from .engine_part import EnginePart
+from . import gas_management, engine_thermo
+from .inlet import Inlet
+
+from ..isa import isa
 
 class Compressor(EnginePart):
-    def __init__(self, inlet_data) -> None:
-        self.inlet_data = inlet_data
+    def __init__(self, compressor_data, inlet: Inlet) -> None:
+        
+        self.inlet = inlet
+        
+        self.compressor_data = compressor_data
 
-        self.stage_number = self.inlet_data["comp_n_stages"]
-        self.compress = self.inlet_data["compress"]
-        self.comp_eta = self.inlet_data["comp_eta"]
-         
+        self.stage_number = self.compressor_data["comp_n_stages"]
+        self.compress = self.compressor_data["compress"]
+        self.comp_eta = self.compressor_data["comp_eta"]
+        
+        self.gas = inlet.gas
+        self.M_comp_in = inlet.M_inlet_out
+
+        self.st_out, convergence, self.compressor_work = engine_thermo.compressor_solver(
+            gas_in=self.gas["2"],
+            n_stages=self.stage_number,
+            compress=self.compress,
+            comp_eta=self.comp_eta,
+            M_in=self.M_comp_in,
+            gas_out=self.gas["3"])
+
+                                                                                         
+
+        
