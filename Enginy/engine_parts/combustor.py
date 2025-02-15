7ebca1755c019a56d6779556425be718c401984d
Patryk Niczke <kyrtap1301@gmail.com>
2025-02-15 21:53:26 +0100

    Add plotting to Combustor class

--- Enginy/engine_parts/combustor.py
+++ Enginy/engine_parts/combustor.py
@@ -1,19 +1,20 @@
+import json
+
 import engine_thermo
 from ..isa import isa
 from .engine_part import EnginePart
 from .inlet import Inlet
 from .compressor import Compressor
 
-from .gas_management import comp_air, comp_fuel
+import gas_management
 
 
 class Combustor(EnginePart):
     """
     Combustor of Jet Engine
     """
-    def __init__(self, combustor_data, inlet: Inlet, compressor: Compressor):
+    def __init__(self, combustor_data, compressor: Compressor):
         
-        self.inlet = inlet
         self.compressor = compressor 
 
         self.combustor_data = combustor_data
@@ -34,7 +35,7 @@ class Combustor(EnginePart):
         
     def _gas_update(self):
         phi = (self.max_fuel - self.max_fuel) * self.throttle_position + self.min_fuel # 
-        self.gas[4].set_equivalence_ratio(phi=phi, fuel=comp_fuel, oxidizer=comp_air, basis='mole')
+        self.gas[4].set_equivalence_ratio(phi=phi, fuel=gas_management.comp_fuel, oxidizer=gas_management.comp_air, basis='mole')
 
         M_calc, conv = engine_thermo.combustor_solver(gas_in=self.gas[3],
                                        V_nominal=self.V_nominal,
@@ -49,6 +50,26 @@ class Combustor(EnginePart):
         
         self.gas[4].equilibrate('HP')
 
-    
+    def analyze(self):
+        compressor_T = []
+        compressor_p = []
+        compressor_X = []
+
+        for x in range(0, 4):
+            compressor_T.append(self.gas[gas_management.st[x]].T)
+            compressor_p.append(self.gas[gas_management.st[x]].P)
+            compressor_X.append(self.gas[gas_management.st[x]].X)
+
+        plot = gas_management.plot_T_s(
+            compressor_T,
+            compressor_p,
+            compressor_X,
+            gas_management.reaction_mechanism,
+            gas_management.phase_name,
+        )
+
+        graphJSON = json.dumps(plot, cls = utils.PlotlyJSONEncoder)
+        return graphJSON
+        
     
     
