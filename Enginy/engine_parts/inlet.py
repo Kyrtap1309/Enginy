a7c5428980d216496225a4ade4945b7ab9d133bc
Patryk Niczke <kyrtap1301@gmail.com>
2025-02-22 19:50:00 +0100

    Add dataclass for compressor data
    Also improve typing and docs

--- Enginy/engine_parts/inlet.py
+++ Enginy/engine_parts/inlet.py
@@ -1,5 +1,5 @@
 import json
-from typing import Union
+from typing import Union, Any
 from dataclasses import dataclass
 from plotly import utils
 
@@ -44,31 +44,31 @@ class Inlet(EnginePart):
             self.inlet_data = inlet_data
 
 
-        self.T_ambient = isa.ISA_T(self.inlet_data.altitude)
-        self.p_ambient = isa.ISA_p(self.inlet_data.altitude)
-        self.M_ambient = self.inlet_data.M_ambient_input
-        self.mass_flow = self.inlet_data.mass_flow
-        self.A_1 = self.inlet_data.A1
-        self.A_2 = self.inlet_data.A2
-        self.inlet_eta = self.inlet_data.eta
-        self.gas = gas_management.initialize_gas(self.T_ambient, self.p_ambient)
+        self.T_ambient: float = isa.ISA_T(self.inlet_data.altitude)
+        self.p_ambient: float = isa.ISA_p(self.inlet_data.altitude)
+        self.M_ambient: float = self.inlet_data.M_ambient_input
+        self.mass_flow: float = self.inlet_data.mass_flow
+        self.A_1: float = self.inlet_data.A1
+        self.A_2: float = self.inlet_data.A2
+        self.inlet_eta: float = self.inlet_data.eta
+        self.gas: dict[Any, Any] = gas_management.initialize_gas(self.T_ambient, self.p_ambient)
 
-        _ambient_stage_index = gas_management.st[0]
+        _ambient_stage_index: str = gas_management.st[0]
         self.ambient_stage_gas = self.gas[_ambient_stage_index]
 
-        self.v_input = self.M_ambient * engine_thermo.get_a(self.ambient_stage_gas)
-        self.gamma = engine_thermo.get_gamma(self.ambient_stage_gas)
-        self.T_total_inlet_in = engine_thermo.get_T_total(
+        self.v_input: float = self.M_ambient * engine_thermo.get_a(self.ambient_stage_gas)
+        self.gamma: float = engine_thermo.get_gamma(self.ambient_stage_gas)
+        self.T_total_inlet_in: float = engine_thermo.get_T_total(
             self.ambient_stage_gas.T, self.gamma, self.M_ambient
         )
         self.p_total_inlet_in = engine_thermo.get_p_total(
             self.ambient_stage_gas.P, self.gamma, self.M_ambient
         )
 
-        _inlet_in_stage_index = gas_management.st[1]
+        _inlet_in_stage_index: int = gas_management.st[1]
         self.inlet_in_stage_gas = self.gas[_inlet_in_stage_index]
 
-        _inlet_out_stage_index = gas_management.st[2]
+        _inlet_out_stage_index: int = gas_management.st[2]
         self.inlet_out_stage_gas = self.gas[_inlet_out_stage_index]
 
         self.mach_inlet_from_ambient_to_entrance()
